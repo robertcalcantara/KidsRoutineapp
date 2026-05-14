1129976efddaa38fbe1d4c5cb943dff3fd314cc7
pedromelounit <p.r.n.m2004@gmail.com>
2026-05-14 13:39:49 -0300

    Add route for new activity screen
    Added a new route for NovaAtividadeScreen in the app.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'tela_recuperar_senha.dart';
 import 'tela_home.dart';
 import 'tela_historico.dart';
 import 'tela_configuracoes.dart';
+import 'tela_nova_atividade.dart;
 
 void main() {
   runApp(
@@ -53,4 +54,64 @@ class KidsRoutineApp extends StatelessWidget {
       },
     );
   }
+=======
+import 'package:flutter/material.dart';
+import 'tela_login.dart';
+import 'tela_cadastro.dart';
+import 'tela_recuperar_senha.dart';
+import 'tela_home.dart';
+import 'tela_historico.dart';
+import 'tela_configuracoes.dart';
+import 'tela_nova_atividade.dart'; // O import da sua nova tela já está aqui!
+
+void main() {
+  runApp(
+    const KidsRoutineApp(),
+  );
+}
+
+class KidsRoutineApp extends StatelessWidget {
+  const KidsRoutineApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kids Routine',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1565C0),
+        ),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        // LOGIN
+        '/': (context) => const TelaLogin(),
+        
+        // CADASTRO
+        '/cadastro': (context) => const TelaCadastro(),
+        
+        // RECUPERAR SENHA
+        '/recuperar-senha': (context) => const TelaRecuperarSenha(),
+        
+        // HOME
+        '/home': (context) => const TelaHome(
+              nomeCrianca: 'Perfil Teste',
+              idCrianca: '#0000',
+            ),
+            
+        // HISTÓRICO
+        '/historico': (context) => const TelaHistorico(),
+        
+        // CONFIGURAÇÕES
+        '/configuracoes': (context) => const TelaConfiguracoes(),
+
+        // NOVA ATIVIDADE
+        '/nova-atividade': (context) => const NovaAtividadeScreen(), // Rota adicionada com sucesso!
+      },
+    );
+  }
+>>>>>>> bfde009 (feat: listagem dinamica de atividades na home)
 }
